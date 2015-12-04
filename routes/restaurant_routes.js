2fc182f67504f451df0363c3ff8015cc7ed5a309
Tina Sharma <tina_2212@hotmail.com>
2015-12-04 15:34:00 -0800

    add code to model, routes, server.js, index.html

---
+++ routes/restaurant_routes.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var Restaurant = require(__dirname + '/../models/restaurant.js');
+var handleError = require(__dirname + '/../lib/handleServerError.js');
+var restaurantsRouter = module.exports = exports = express.Router();
+
+restaurantsRouter.get('/restaurants', function(req, res) {
+  Restaurant.find({},
+  function(err, data) {
+    if (err) return handleError(err, res);
+    res.json(data);
+  });
+});
+
+restaurantsRouter.post('/restaurants', bodyParser(), function(req,res) {
+  var newRestaurant = new Restaurant(req.body);
+  newRestaurant.save(function(err, data) {
+    if (err) return handleError(err, res);
+    res.json(data);
+  });
+});
+
+restaurantsRouter.put('/restaurants/:id', bodyParser.json(), function(req, res) {
+  var restaurantData = req.body;
+  delete restaurantData._id;
+  Restaurant.update({_id: req.params.id}, restaurantData, function(err) {
+    if (err) return handleError(err, res);
+    res.json({msg: 'success!'});
+  });
+});
+
+restaurantsRouter.delete('/restaurants/:id', function(req, res) {
+  Restaurant.remove({_id: req.params.id}, function(err) {
+    if (err) return handleError(err, res);
+    res.json({msg: 'success!'});
+  });
+});
---
+++ server.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+var express = require('express');
+var app = express();
+
+var restaurantsRouter = require(__dirname + '/routes/restaurants_routes.js');
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/restaurant_stream_dev');
+
+app.use('/api', restaurantsRouter);
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log('server is running');
+});
