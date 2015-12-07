db88b404a5033bfca9e7e3a953f3d3f0bce1a9b8
Tina Sharma <tina_2212@hotmail.com>
2015-12-06 16:24:14 -0800

    add index.html route to server.js

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 var mongoose = require('mongoose');
 var express = require('express');
 var app = express();
+var path = require('path');
 
 var restaurantsRouter = require(__dirname + '/routes/restaurant_routes.js');
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/restaurant_stream_dev');
 
 app.use('/api', restaurantsRouter);
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 app.listen(process.env.PORT || 3000, function() {
   console.log('server is running');
